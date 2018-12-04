c1ee7a0b14dbbead481c317c5e4e66ba1c78c2a0
Kevin Q <kevinqiu1@gmail.com>
2018-12-04 00:41:26 -0500

    updaes

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -16,11 +16,11 @@ main = do
     } `additionalKeys`
     [ ((0, xF86XK_MonBrightnessUp ), spawn "xbacklight +10")
     , ((0, xF86XK_MonBrightnessDown ), spawn "xbacklight -10")
-    , ((0, xF86XK_AudioRaiseVolume ), spawn "amixer set Master 2+")
-    , ((0, xF86XK_AudioLowerVolume ), spawn "amixer set Master 2-")
-    , ((0, xF86XK_AudioMute ), spawn "amixer -D pulse set Master toggle")
+    , ((0, xF86XK_AudioRaiseVolume ), spawn "pamixer -i 5")
+    , ((0, xF86XK_AudioLowerVolume ), spawn "pamixer -d 5")
+    , ((0, xF86XK_AudioMute ), spawn "pamixer -t")
     ]
 
-myTerminal    = "gnome-terminal"
+myTerminal    = "urxvt"
 myModMask     = mod4Mask -- Win key or Super_L
 myBorderWidth = 3
